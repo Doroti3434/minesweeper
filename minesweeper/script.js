68ae5defdd6bc3cd1d8f7ebc8968f94d776a08cb
Daria Nazarenko <97506749+Doroti3434@users.noreply.github.com>
2023-02-13 10:22:14 +0300

    Add files via upload

---
+++ minesweeper/script.js
@@ -0,0 +1,58 @@
+var board = [];
+var rows = 8;
+var columns = 8;
+
+var minesCount = 10;
+var minesLocation = []; // "2-2", "3-4", "2-1"
+
+var tilesClicked = 0; //goal to click all tiles except the ones containing mines
+var flagEnabled = false;
+
+var gameOver = false;
+
+window.onload = function() {
+    startGame();
+}
+
+function startGame() {
+    document.getElementById("mines-count").innerText = minesCount;
+    document.getElementById("flag-button").addEventListener("click", setFlag);
+
+    //заполняем дощечку
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click", clickTile);
+            document.getElementById("board").append(tile);
+            row.push(tile);
+        } 
+        board.push(row);
+    }
+    console.log(board);
+}
+
+function setFlag() {
+    if (flagEnabled) {
+        flagEnabled = false;
+        document.getElementById("flag-button").style.backgroundColor = "lightgray";
+    }
+    else {
+        flagEnabled = true;
+        document.getElementById("flag-button").style.backgroundColor = "darkgray";
+    }
+}
+
+function clickTile(){
+    let tile = this;
+    if (flagEnabled) {
+        if (tile.innerText == "") {
+            tile.innerText = "🚩";
+        }
+        else if (tile.innerText == "🚩") {
+            tile.innerText = "";
+        }
+        return;
+    }
+}
